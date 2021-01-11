66e88259c4a01b2a30d861188a483128906ba47d
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-11 01:09:46 -0300

    Finalizando aula de operadores

---
+++ operadores.js
@@ -0,0 +1,30 @@
+var soma = 10 + 10;
+console.log(soma);
+
+soma++ //adiciona + 1, ou seja, 20 +1
+console.log(soma);
+
+soma++//adiciona + 1, ou seja, 21 +1
+console.log(soma);
+
+++soma
+
+console.log(soma);
+
+
+console.log("------------------------------------------");
+
+soma = soma -1
+
+soma--
+console.log(soma);
+soma--
+console.log(soma);
+
+console.log("------------------------------------------");
+
+soma += 20
+console.log(soma); //SOMA = SOMA +20;
+
+soma -= 20
+console.log(soma); //SOMA =SOMA -20
