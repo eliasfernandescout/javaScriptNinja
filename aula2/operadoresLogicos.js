c6964f4ea8fd92a42fb1ef3fd9faa86dac75ed2e
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-12 09:51:58 -0300

    Finalizando a segunda aula

---
+++ aula2/operadoresLogicos.js
@@ -0,0 +1,11 @@
+var x = 3;
+var y = 7;
+
+console.log(x===3);
+console.log(y===5);
+
+console.log(x===3 && y===7); //SO RETORNA TRUE QUANDO OS 2 VALORES SÃO VERDADEIROS
+console.log(x===3 || y===5); //RETORNA TRUE SE UM DOS 2 FOREM VERDADEIROS
+
+console.log(x != 3);//NEGAÇÃO
+
