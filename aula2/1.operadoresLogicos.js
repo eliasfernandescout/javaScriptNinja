5827b7ddba310fe6b270a90570156107cd0e3db2
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-05-26 16:22:30 -0300

    aula operadores unarios

--- aula2/1.operadoresLogicos.js
+++ aula2/1.operadoresLogicos.js
@@ -2,10 +2,10 @@ var x = 3;
 var y = 7;
 
 console.log(x===3);
-console.log(y===5);
+console.log(y===5, "\n");
 
 console.log(x===3 && y===7); //SO RETORNA TRUE QUANDO OS 2 VALORES SÃO VERDADEIROS
 console.log(x===3 || y===5); //RETORNA TRUE SE UM DOS 2 FOREM VERDADEIROS
 
-console.log(x != 3);//NEGAÇÃO
+console.log(x === 3);//NEGAÇÃO
 
