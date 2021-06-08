014fbd264d1d6987169e51f2fda089cd89918738
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-06-07 23:49:26 -0300

    semana 4

---
+++ aula4/2.condicionalTernario.js
@@ -0,0 +1,36 @@
+// console.log(
+//     5 > 2 || 3 > 7 ? '5 é maior que 2 e 3 é menor que 7' : 'tente outra vez'
+
+
+//     );
+
+
+    pessoa ={
+        nome: 'Jazz',
+        sexo: 'Feminino'     
+    }
+
+    var sexoF = 'a'
+
+    let imprimir = pessoa.sexo === 'Feminino' ? `Olá, eu sou ${sexoF} Eliza ` : `Olá, eu sou o Elias`
+
+    console.log(imprimir)
+
+    let imprimir2 = pessoa.nome === 
+    'Dante' 
+    ?
+    `Olá, eu sou o ${pessoa.nome}, bem vindo ao meu workspace`
+    :
+    pessoa.nome === 'Jhessiny'
+    ?
+    `Meu nome é ${pessoa.nome}`
+    :
+    pessoa.nome === 'Elias'
+    ?
+    `${pessoa.nome} é um cara legal `
+    :
+    'Tente novamente, até acertar os nomes corretos'
+
+    console.log(imprimir2)
+
+   
---
+++ aula4/3.escopoDeVariaveis.js
@@ -0,0 +1,16 @@
+var myVar = 1;
+
+console.log(myVar)
+
+function myFunction(){
+    return myVar
+}
+
+console.log(myFunction())
+
+function otherFunction(){
+    var otherVar = true;
+    return otherVar;
+}
+
+console.log(otherFunction())
