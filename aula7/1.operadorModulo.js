be0425903038ff3d11cff85da56d8c4f32a2b321
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-06-26 21:14:26 -0300

    operador modulo

---
+++ aula7/1.operadorModulo.js
@@ -0,0 +1,18 @@
+// RESTO DA DIVISÃO
+
+// var num1 = 3
+// var num2 = 3
+
+// var divisao = num1/num2
+// console.log(divisao)
+
+var num1 = 5
+var num2 = 2
+
+var divisao = num1/num2
+console.log(divisao)
+
+//CASO QUEIRA SABER O RESTO DA DIVISAO USAR-SE O OPERADOR %
+
+var divComOperador = num1%num2
+console.log(divComOperador)
