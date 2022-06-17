bea3565b1b70c4f9a7d7407c4847fdd95f61ebf9
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-17 17:07:37 -0300

    add saltos js

---
+++ 11-loops/saltos.js
@@ -0,0 +1,10 @@
+const minhaFunc = () =>{
+    var number = 6;
+    
+    const verifyNumber = number === 10 ? true : false
+    return  verifyNumber
+}
+
+console.log(minhaFunc())
+
+/* o RETURN pula a execucao das proximas etapas */
