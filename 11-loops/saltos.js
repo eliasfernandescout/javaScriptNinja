5279ee701de9d8d94cd275ea69f1cec063dde5e2
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-18 09:23:51 -0300

    insert switch in saltos

--- 11-loops/saltos.js
+++ 11-loops/saltos.js
@@ -8,3 +8,23 @@ const minhaFunc = () =>{
 console.log(minhaFunc())
 
 /* o RETURN pula a execucao das proximas etapas */
+
+var number = 10;
+
+switch(number){
+    case 1: 
+        console.log('1')
+        break;
+    case 2:
+        console.log('2')
+        break;
+    case 10:
+        console.log('10')
+        break;
+
+    default:
+        console.log('default')
+
+}
+
+console.log('Fim do switch')
